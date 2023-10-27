dbc82df602ce73cbc03185f988edd96a0aae8a93
alaminces <alaminces6@gmail.com>
2023-10-27 10:30:47 +0600

    modified

--- functions.php
+++ functions.php
@@ -36,29 +36,29 @@ function getUserRegistrationInfo($input,$filename, &$errorMsg) {
 		$errorMsg = "Field Must Not be Empty!";
 	}else {
 	
-	if ( !file_exists($filename) ) {
-		$errorMsg = "{$filename} is not exist!";
-		exit;
-	}
-	
-	$fileData = file($filename);
-	$existEmail = false;
-	foreach($fileData as $data) {
-		list($newRole,$newUsername,$newEmail,$newPassword) = explode(",",trim($data));
-		if ( $newEmail == $email ) {
-			$existEmail = true;
+		if ( !file_exists($filename) ) {
+			$errorMsg = "{$filename} is not exist!";
+			exit;
 		}
-	}
 	
-	if ( $existEmail ) {
-		$errorMsg = "Already email registered. Please provide another email!";
-	}else {
-		$fp = fopen($filename,"a");
-		$data = sprintf("%s,%s,%s,%s\n","user",$username,$email,$password);
-		fwrite($fp,$data);
-		header("Location:login.php");
-		fclose($fp);
-	}
+		$fileData = file($filename);
+		$existEmail = false;
+		foreach($fileData as $data) {
+			list($newRole,$newUsername,$newEmail,$newPassword) = explode(",",trim($data));
+			if ( $newEmail == $email ) {
+				$existEmail = true;
+			}
+		}
+	
+		if ( $existEmail ) {
+			$errorMsg = "Already email registered. Please provide another email!";
+		}else {
+			$fp = fopen($filename,"a");
+			$data = sprintf("%s,%s,%s,%s\n","user",$username,$email,$password);
+			fwrite($fp,$data);
+			header("Location:login.php");
+			fclose($fp);
+		}
 	}
 }
 
@@ -73,7 +73,6 @@ function userLoginProcess($input,$filename, &$errorMsg) {
 	if ($email=="" || $password=="") {
 		$errorMsg = "Field Must Not be Empty!";
 	}else {
-	
 		$fileData = file($filename);
 		
 		$isLogined = false;
@@ -104,10 +103,7 @@ function userLoginProcess($input,$filename, &$errorMsg) {
 		}
 	}
 }
-
-
 /***************  Login Process End  ****************/
-
 
 
 /***************  Display Users Info Start  ****************/
@@ -115,7 +111,6 @@ function displayUsersInfo($filename) {
 	return  file($filename);
 }
 /***************  Display Users Info End  ****************/
-
 
 /***************  Delete User Start  ****************/
 function deleteUser($filename,$delId) {
@@ -136,7 +131,6 @@ function deleteUser($filename,$delId) {
 }
 /***************  Delete User End  ****************/
 
-
 /*************** Update Process Start  ****************/
 function updateUserInfo($input,$filename, &$errorMsg,$editId) {
 	$username = $input['username'];
@@ -146,47 +140,45 @@ function updateUserInfo($input,$filename, &$errorMsg,$editId) {
 	if ( $username=="" || $email=="" || $role=="" ) {
 		$errorMsg = "Field Must Not be Empty!";
 	}else {
-	
-	if ( !file_exists($filename) ) {
-		$errorMsg = "{$filename} is not exist!";
-		exit;
-	}
-	
-	$fileData = file($filename);
-	$existEmail = false;
-	$emails = [];
-	$oldPass = "";
-	foreach($fileData as $data) {
-		list($newRole,$newUsername,$newEmail,$newPassword) = explode(",",trim($data));
-		$oldPass = $newPassword;
-		if ( $newEmail == $email ) {
-			array_push($emails,$newEmail);
-			$existEmail = true;
+		if ( !file_exists($filename) ) {
+			$errorMsg = "{$filename} is not exist!";
+			exit;
 		}
-	}
 	
-	$totalEmail = count($emails);
-
-	if ( $existEmail && $totalEmail > 1 ) {
-		$errorMsg = "Already email registered. Please provide another email!";
-	}else {
-		unset($fileData[$editId]);
-		
-		$fp = fopen($filename,"w");
+		$fileData = file($filename);
+		$existEmail = false;
+		$emails = [];
+		$oldPass = "";
 		foreach($fileData as $data) {
-			fwrite($fp,$data);
+			list($newRole,$newUsername,$newEmail,$newPassword) = explode(",",trim($data));
+			$oldPass = $newPassword;
+			if ( $newEmail == $email ) {
+				array_push($emails,$newEmail);
+				$existEmail = true;
+			}
 		}
-		fclose($fp);
-		
-		$fp = fopen($filename,"a");
-		$data = sprintf("%s,%s,%s,%s\n",$role,$username,$email,$oldPass);
-		fwrite($fp,$data);
-		header("Location:login.php");
-		fclose($fp);
-	}
+	
+		$totalEmail = count($emails);
+
+		if ( $existEmail && $totalEmail > 1 ) {
+			$errorMsg = "Already email registered. Please provide another email!";
+		}else {
+			unset($fileData[$editId]);
+			
+			$fp = fopen($filename,"w");
+			foreach($fileData as $data) {
+				fwrite($fp,$data);
+			}
+			fclose($fp);
+			
+			$fp = fopen($filename,"a");
+			$data = sprintf("%s,%s,%s,%s\n",$role,$username,$email,$oldPass);
+			fwrite($fp,$data);
+			header("Location:login.php");
+			fclose($fp);
+		}
 	}
 }
-
 /***************  Update Process End  ****************/
 
 /***************  Add New Process Start  ****************/
@@ -195,7 +187,6 @@ function addNewUser($filename,$input,&$errorMsg) {
 	$email 	   = $input['email'];
 	$role  		   = $input['role'] ?? "";
 	$password = $input['password'];
-	
 
 	if ($username=="" || $email=="" || $role=="" || $password=="" ) {
 		$errorMsg = "Field must not be empty!";
@@ -217,8 +208,7 @@ function addNewUser($filename,$input,&$errorMsg) {
 			header("Location:index.php");
 			fclose($fp);
 		}
-	
 	}
 }
 
-/***************  Add New Process Start  ****************/
+/***************  Add New Process End  ****************/
